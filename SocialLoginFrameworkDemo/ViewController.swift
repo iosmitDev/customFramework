b5b84e14368e762510dd15527767dc4963cfeacd
Mitesh <mitesh.imply@gmail.com>
2023-07-22 18:30:45 +0530

    Initial Commit

---
+++ SocialLoginFrameworkDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLoginFrameworkDemo
+//
+//  Created by MiteshKumar Patel on 22/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
