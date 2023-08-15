eb2939c8abeb87e6b08d6eed0e523d4d2f204c07
Mitesh <mitesh.imply@gmail.com>
2023-08-15 13:32:04 +0530

    Commit -m "framework Committed"

---
+++ SocialLogin.xcframework/ios-arm64/SocialLogin.framework/Headers/SocialLogin.h
@@ -0,0 +1,18 @@
+//
+//  SocialLogin.h
+//  SocialLogin
+//
+//  Created by MiteshKumar Patel on 22/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialLogin.
+FOUNDATION_EXPORT double SocialLoginVersionNumber;
+
+//! Project version string for SocialLogin.
+FOUNDATION_EXPORT const unsigned char SocialLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialLogin/PublicHeader.h>
+
+//#import <SocialLogin/socialData.h>
