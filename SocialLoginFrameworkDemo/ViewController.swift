4e4f50eca33e35148333c1f11892540900883557
miteshpate-admin <Mitesh.imply@gmail.com>
2024-07-25 02:29:14 +0530

    first commit

--- SocialLoginFrameworkDemo/ViewController.swift
+++ SocialLoginFrameworkDemo/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import SocialLogin
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let responseFromFramework = socialData()
+        let getLoginName = responseFromFramework.getLoginName()
+        
+        print(getLoginName)
     }
-
 
 }
 
